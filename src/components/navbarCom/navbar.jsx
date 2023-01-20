1f62a534f001220b57b65505c5da01b335304af9
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-01-21 00:23:55 +0530

    nav bar

--- src/components/navbarCom/navbar.jsx
+++ src/components/navbarCom/navbar.jsx
@@ -4,12 +4,35 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <h3>Modelyn Torfy</h3>
-      <div className="icon">
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-        </div>  
-      <ul>
+      {/* <div className='icon'>
+        <div>
+          <div className='line'></div>
+          <div className='line'></div>
+          <div className='line'></div>
+
+          <div className='mobile-menu'>
+            <p>
+              <a href='#'>About</a>
+            </p>
+            <p>
+              <a href='#'>Projects</a>
+            </p>
+            <p>
+              <a href='#'>Contact</a>
+            </p>
+          </div>
+        </div>
+      </div> */}
+      <div className='dropdown'>
+        <button className='dropbtn'>Dropdown</button>
+        <div className='dropdown-content'>
+          <a href='#'>Link 1</a>
+          <a href='#'>Link 2</a>
+          <a href='#'>Link 3</a>
+        </div>
+      </div>
+
+      <ul className='desktop-menu'>
         <li>
           <a href='#'>About</a>
         </li>
